dfbf756c67a8e849fd136cd01e759b41ef531036
veronicapuddu <veronica.puddu@vodafone.com>
2025-12-03 12:09:05 +0100

    change project

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { k } from "./kaboomCtx"
-
-k.loadSprite("spritesheet", "./spritesheet.png", {
-    sliceX: 39, //numero frame nel file sprite 16x16
-    sliceY: 31,
-    anims: {  //nomi specifici per le animazioni (usare tiled)
-
-    }
-})
